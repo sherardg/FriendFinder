c70aed3928aab9e415da7533157bc3cc621b62aa
Sherard <sherardg@pennmedicine.upenn.edu>
2019-07-24 23:09:23 -0400

    Updates to survey and routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,19 @@
 //A GET route with the url /api/friends. This will be used to display a JSON of all possible friends.
 
-var friendData = require("..data/friends", function(req, res){
-    res.json(friendData);
-});
+var tableData = require("../data/friendData");
 
-app.post("api/friends", function(req, res){
-    res.send("Hello world");
-});
+module.exports = function(app) {
+    // API GET Requests
+    // Below code handles when users "visit" a page.
+    // In each of the below cases when a user visits a link
+    // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+    // ---------------------------------------------------------------------------
+  
+    app.get("/api/friends", function(req, res) {
+      res.json(friendData);
+    });
+
+    app.post("api/friends", function(req, res){
+        res.json(friendData);
+    })
+}
--- server.js
+++ server.js
@@ -11,6 +11,15 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 
 // LISTENER
 // The below code effectively "starts" our server
